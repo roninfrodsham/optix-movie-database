af50efd6caa572990f43c5a9a4210854855a976e
roninfrodsham <roninfrodsham@gmail.com>
2024-03-17 17:02:53 +0000

    creat basic project structure and add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+const api_url = import.meta.env.VITE_API_URL;
+
 export const App = () => {
+  console.log(api_url);
   return (
     <div>
-      <h1>App</h1>
+      <h1>Welcome to the Movie Database!</h1>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { App } from "./App.tsx";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Outfit, sans-serif",
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          padding: "2em",
+        },
+      },
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
